1bf193ce7d527060fca4422ec038685d50058166
Dmitry Druzhkov <dmitriy.druzhkov@gmail.com>
2020-01-03 22:50:04 +0300

    added NodeJS with mongoDB

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET Userlist page. */
+router.get('/userlist', function (req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({}, {}, function (e, docs) {
+    res.render('userlist', {
+      "userlist": docs
+    });
+  });
+});
+
+/* GET users listing. */
+router.get('/user', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+  res.render('helloworld', { title: 'Hello, World!' });
+});
+
+module.exports = router;
